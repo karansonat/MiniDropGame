dcb94f33f6f15bc1b43a563f4f7ff0c08fd176dd
Sonat Karan <sonat.karan@infosfer.com>
2016-05-26 13:46:38 +0300

    initial tile based terrain creation

---
+++ Assets/Scripts/LevelConfig.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelConfig : MonoBehaviour
+{
+    [Header("Level Config")]
+    public int row;
+    public int col;
+
+    [Header("Prefabs")]
+    public GameObject Tile;
+
+	// Use this for initialization
+	void Start () {
+	    InitLevel();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void InitLevel()
+    {
+        CreateTerrain();
+    }
+
+    private void CreateTerrain()
+    {
+        var levelContainer = GameObject.Find("Level");
+        //Find middle point of the board.
+        var tileWidth = Tile.GetComponent<Renderer>().bounds.size.x;
+        Debug.Log(tileWidth);
+        var topLeftCornerPos = new Vector3(-(tileWidth * col / 2), 0, (tileWidth * row / 2));
+
+        for (var r = 0; r < row; r++)
+        {
+            for (var c = 0; c < col; c++)
+            {
+                var tile = Instantiate(Tile);
+                tile.name = "Tile {" + r + " , " + c + "}";
+                var tileComp = tile.GetComponent<Tile>();
+                tileComp._row = r;
+                tileComp._col = c;
+                tile.transform.SetParent(levelContainer.transform);
+                //Set tile's position.
+                tile.transform.localPosition = new Vector3(topLeftCornerPos.x + (c * tileWidth), 0, topLeftCornerPos.z + (r * -tileWidth));
+            }
+        }
+        levelContainer.transform.position = Vector3.zero;
+    }
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Tile : MonoBehaviour
+{
+    public int _row;
+    public int _col;
+    
+    private bool _isHighlighted;
+
+    public void Init()
+    {
+        _isHighlighted = false;
+
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    void OnMouseEnter()
+    {
+        Debug.Log("Tile : " + gameObject.name);
+    }
+
+    void OnMouseExit()
+    {
+        
+    }
+}
