28fff76af49745b44b99a23302a8d42efd949600
Sonat Karan <sonat.karan@infosfer.com>
2016-05-27 23:43:44 +0300

    wip

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,12 +6,17 @@ public class GameController : MonoSingleton<GameController>
 {
 
     private Level _level;
+    private Material _grassMaterial;
+    private Material _higlightedGrassMaterial;
+
     public Tile _selectedTile;
 
 	void Start () {
         _level = new Level();
 	    LevelConfig.Instance.InitLevel();
-	}
+        _grassMaterial = Resources.Load("Materials/Grass") as Material;
+        _higlightedGrassMaterial = Resources.Load("Materials/HighlightedMaterial") as Material;
+    }
 	
 	void Update () {
 	
@@ -26,8 +31,24 @@ public class GameController : MonoSingleton<GameController>
     {
         if (_selectedTile)
         {
+            UnHighlightTile();
             _selectedTile.DeselectTile();
         }
         _selectedTile = tile;
+        HighlightTile();
+    }
+
+    private void HighlightTile()
+    {
+        var materials = _selectedTile.gameObject.GetComponent<MeshRenderer>().materials;
+        materials[materials.Length - 1] = _higlightedGrassMaterial;
+        _selectedTile.gameObject.GetComponent<MeshRenderer>().materials = materials;
+    }
+
+    private void UnHighlightTile()
+    {
+        var materials = _selectedTile.gameObject.GetComponent<MeshRenderer>().materials;
+        materials[materials.Length - 1] = _grassMaterial;
+        _selectedTile.gameObject.GetComponent<MeshRenderer>().materials = materials;
     }
 }
